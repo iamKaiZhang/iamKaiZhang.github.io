2ea79399cfd825770f2cf8ab1b51b8d6f13a3fa2
Kai Zhang <zhkai2020@163.com>
2025-11-12 14:53:06 +0100

    published: new project

--- src/data/teaching.ts
+++ src/data/teaching.ts
@@ -1,12 +1,13 @@
 export const teachingMarkdown = `# Courses
-- [**Game Theory and Control**](https://www.bsaver.io/teaching/game-theory-and-control) - Dr. Saverio Bolognani, ETH Zurich  
+- **[Game Theory and Control](https://www.bsaver.io/teaching/game-theory-and-control) - Dr. Saverio Bolognani, ETH Zürich**  
 *Fall 2023, Fall 2025*  
 Duties: Tutorial sessions, Exam preparation
-- **Control Systems** - Prof. Dr. Florian Dörfler, ETH Zurich  
+- **Control Systems - Prof. Dr. Florian Dörfler, ETH Zürich**  
 *Spring 2025*  
 Duties: Exam preparation
 
 
 # Student Supervision
+- **<span style="color:#ff4500;">[OPEN PROJECT]</span>**, "[The “Give-and-Take” in Recommender Systems: A Karma Economy for Fair Exploration](https://sirop.org/app/3bcb1822-be85-4a63-b37b-275a4a695112)", with Stefania Ionescu
 - Luise Lenné, "CO2 Karma Police" (Master Thesis 2025), with [Ezzat Elokda](https://www.elokda.info) and [Saverio Bolognani](https://www.bsaver.io)
 `;
