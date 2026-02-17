8ec2d1099b539205177fd545c8b8d7d07a8e87a1
Kai Zhang <zhkai2020@163.com>
2026-02-17 16:58:48 +0100

    updated: teaching

--- src/data/teaching.ts
+++ src/data/teaching.ts
@@ -8,9 +8,8 @@ Duties: Exam preparation
 
 
 # Student Supervision
-- Sophia Lahrech, "Strategy-proof Learning in Refugee Allocation" (Semester Project 2026), with Stefania Ionescu
-- Idriss Benhallam, "Fair Redesign of Online Ad Auctions with a Karma Econ-
-omy" (Semester Project 2026), with Stefania Ionescu
-- Kim Kleinlogel, "The “Give-and-Take” in Recommender Systems: A Karma Economy for Fair Exploration" (Bachelor Thesis 2026), with Stefania Ionescu
-- Luise Lenné, "CO2 Karma Police" (Master Thesis 2025), with [Ezzat Elokda](https://www.elokda.info) and [Saverio Bolognani](https://www.bsaver.io)
+- **<span style="color:#548235;">[ONGOING]</span>** **Sophia Lahrech**, *"Strategy-proof Learning in Refugee Allocation"* (Semester Project 2026), with Stefania Ionescu
+- **<span style="color:#548235;">[ONGOING]</span>** **Idriss Benhallam**, *"Fair Redesign of Online Ad Auctions with a Karma Economy"* (Semester Project 2026), with Stefania Ionescu
+- **<span style="color:#548235;">[ONGOING]</span>** **Kim Kleinlogel**, *"The “Give-and-Take” in Recommender Systems: A Karma Economy for Fair Exploration"* (Bachelor Thesis 2026), with Stefania Ionescu
+- **Luise Lenné**, *"CO2 Karma Police"* (Master Thesis 2025), with [Ezzat Elokda](https://www.elokda.info) and [Saverio Bolognani](https://www.bsaver.io)
 `;
